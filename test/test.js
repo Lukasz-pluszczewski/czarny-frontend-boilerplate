60a1d2b85e62725c90b24340f5301d0150e65bc1
Adam <adam@brainhub.pl>
2016-11-17 15:05:10 +0100

    test coverage configured

---
+++ test/test.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import React from 'react';
+import { shallow } from 'enzyme';
+import NotFoundPage from '../src/components/NotFoundPage.js';
+
+describe('Test', () => {
+  it('NotFound Page', () => {
+    const wrapper = shallow(<NotFoundPage />);
+    expect(wrapper.find('h4').text()).to.equal('404 Page Not Found');
+  });
+});
