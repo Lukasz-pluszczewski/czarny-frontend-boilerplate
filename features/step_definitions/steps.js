17bb2a6c72e622b372c0992b1e3ef979820ab5c3
Adam <adam@brainhub.pl>
2016-11-17 12:56:58 +0100

    added and configured cucumber, nightwatch, selenium

---
+++ features/step_definitions/steps.js
@@ -0,0 +1,15 @@
+module.exports = function() {
+  this.Given(/^I open Google's search page$/, (client) => {
+    client
+      .url('http://google.com')
+      .waitForElementVisible('body', 1000);
+  });
+
+  this.Then(/^the title is "([^"]*)"$/, (client, title) => {
+    client.assert.title(title);
+  });
+
+  this.Then(/^the Google search form exists$/, (client) => {
+    client.assert.visible('input[name="q"]');
+  });
+};
