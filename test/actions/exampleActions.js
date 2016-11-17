aea232d08fc0cd269b367cfe1e45a052299d1775
lukasz_brainhub <lukasz@brainhub.pl>
2016-11-17 15:43:46 +0100

    exampleAction test

---
+++ test/actions/exampleActions.js
@@ -0,0 +1,26 @@
+import chai, { expect } from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import { exampleAction } from 'actions/exampleActions';
+import {
+  EXAMPLE_ACTION,
+  EXAMPLE_ACTION_SUCCESS,
+  EXAMPLE_ACTION_ERROR
+} from 'constants/actionTypes';
+
+chai.use(chaiAsPromised);
+
+describe('ExampleAction creator', () => {
+  it('should create example action', (done) => {
+    const name = 'testName';
+    const actionTypes = [
+      EXAMPLE_ACTION,
+      EXAMPLE_ACTION_SUCCESS,
+      EXAMPLE_ACTION_ERROR
+    ];
+    const action = exampleAction(name);
+
+    expect(action.types).to.deep.equal(actionTypes);
+    expect(action.promise).to.be.a('function');
+    expect(action.promise(name)).to.be.eventually.equal(name).notify(done);
+  });
+});
