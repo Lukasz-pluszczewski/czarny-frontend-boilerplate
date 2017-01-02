f0b824a586c6f7f374c53ce480b111d26afa8492
Adam <adam@brainhub.pl>
2017-01-02 16:51:16 +0100

    added tests in cucumber in wdio

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,15 +1,15 @@
 module.exports = function() {
-  this.Given(/^I open Google's search page$/, client => {
-    client
+  this.When(/^I open Google's search page$/, () => {
+    browser
       .url('http://google.com')
-      .waitForElementVisible('body', 1000);
+      .waitForVisible('body', 1000);
   });
 
-  this.Then(/^the title is "([^"]*)"$/, (client, title) => {
-    client.assert.title(title);
+  this.Then(/^the title is "([^"]*)"$/, title => {
+    expect(browser.getTitle()).to.be.equal(title);
   });
 
   this.Then(/^the Google search form exists$/, client => {
-    client.assert.visible('input[name="q"]');
+    expect(browser.isVisible('input[name="q"]')).to.be.equal(true);
   });
 };
