eb1b2e0f5633e363727ffeeb41d3af23eee0cdb2
Adam <adam@brainhub.pl>
2017-05-12 11:31:58 +0200

    Brainhub logo in ASCII

--- tools/startMessage.js
+++ tools/startMessage.js
@@ -1,4 +1,15 @@
 import { chalkSuccess } from './chalkConfig';
 
 /* eslint-disable no-console */
+
+console.log('                    #######                ##          ##                ##     ');
+console.log('##  ##########      ##   ##                ##          ##                ##     ');
+console.log('##  ##########      ##    ##                           ##                ##     ');
+console.log('                    ##   ##   ####  ####   ##   #####  ######   ##   ##  #####  ');
+console.log('  #######  ###      #######  ####  ##  ##  ##  ##  ##  ##   ##  ##   ##  ##  ## ');
+console.log('  #######  ###      #######  ##   ##   ##  ##  ##  ##  ##   ##  ##   ##  ##   ##');
+console.log('                    ##    ## ##   ##   ##  ##  ##  ##  ##   ##  ##   ##  ##   ##');
+console.log('####  ##  ####      ##    ## ##   ##   ##  ##  ##  ##  ##   ##  ##   ##  ##   ##');
+console.log('####  ##  ####      ##   ##  ##    ##  ##  ##  ##  ##  ##   ##   ## ##   ##  ## ');
+console.log('                    ######   ##     #####  ##  ##  ##  ##   ##    ###    #####  ');
 console.log(chalkSuccess('Starting app in dev mode...'));
