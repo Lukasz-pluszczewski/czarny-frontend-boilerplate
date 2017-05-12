f452b95952d430a4f569e53b6d602a5665c4678f
Adam <adam@brainhub.pl>
2017-05-12 10:21:08 +0200

    change node version to 7

--- tools/nodeVersionCheck.js
+++ tools/nodeVersionCheck.js
@@ -4,7 +4,7 @@ var exec = require('child_process').exec;
 exec('node -v', function (err, stdout) {
   if (err) throw err;
 
-  if (parseFloat(stdout.slice(1)) < 6) {
-    throw new Error('This app requires node 6.0 or greater.');
+  if (parseFloat(stdout.slice(1)) < 7) {
+    throw new Error('This app requires node 7.0 or greater.');
   }
 });
